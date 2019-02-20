e9fb614d3994946bbe71acd5168cbacc2ad6f675
Igor <natak@zahav.net.il>
2019-02-20 19:15:14 +0200

    Updates

---
+++ precache-manifest.210a870bb7d2571d8edf892581997fb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4bf40f27ed8dbd6f9da",
+    "url": "/contactmanager/static/js/runtime~main.e4bf40f2.js"
+  },
+  {
+    "revision": "9f8b6b67f70fff72f7ad",
+    "url": "/contactmanager/static/js/main.9f8b6b67.chunk.js"
+  },
+  {
+    "revision": "71854edce4c9ce04eda9",
+    "url": "/contactmanager/static/js/2.71854edc.chunk.js"
+  },
+  {
+    "revision": "9f8b6b67f70fff72f7ad",
+    "url": "/contactmanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "71854edce4c9ce04eda9",
+    "url": "/contactmanager/static/css/2.e6b93598.chunk.css"
+  },
+  {
+    "revision": "b93571325484ed122405c2e7f59476cf",
+    "url": "/contactmanager/index.html"
+  }
+];
