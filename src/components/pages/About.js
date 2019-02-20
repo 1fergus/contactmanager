b6071466f1c53bf3f1edfd10092c0a26beb35487
Igor <natak@zahav.net.il>
2019-02-20 19:06:56 +0200

    first commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h1 className="display-4">About contact manager</h1>
+      <p className="lead">To be or not to be an be the beeee</p>
+      <p className="text-secondary">Version 1</p>
+    </div>
+  );
+}
